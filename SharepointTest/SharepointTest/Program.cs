ed9be4f6d16ff83bf3b678d8c042db1f2bb195c8
FulopValeria <fulop.valeria@netlock.hu>
2016-01-07 11:49:39 +0100

    Trying to remove

--- SharepointTest/SharepointTest/Program.cs
+++ SharepointTest/SharepointTest/Program.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
 
 namespace SharepointTest {
     class Program {
         static void Main(string[] args) {
+            
         }
     }
 }
